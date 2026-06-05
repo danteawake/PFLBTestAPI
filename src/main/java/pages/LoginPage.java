93b9d14e00e41079f52918d972b11c22f3f2d5b5
Oleg Egorov <danteawake88@gmail.com>
2026-06-05 13:55:51 +0400

    First test BaseTest.java + pom: Allure Selenide Junit

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,25 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class LoginPage {
+
+    private final SelenideElement emailInput = $(byName("email"));
+    private final SelenideElement passwordInput = $(byName("password"));
+    private final SelenideElement signInButton = $(byTagAndText("button", "GO"));
+
+    public void openPage() {
+        open("");
+    }
+
+    public void login(String email, String password) {
+        emailInput.setValue(email);
+        passwordInput.setValue(password);
+        signInButton.click();
+    }
 }
