51742e6e15744e4db3d764ea72c6584761e9c54e
Oleg Egorov <danteawake88@gmail.com>
2026-06-05 18:24:12 +0400

    Junit -> TestNG  + spaces fix

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,9 +5,9 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
 import pages.UsersReadAllPage;
 
@@ -18,7 +18,7 @@ public class BaseTest {
     protected LoginPage loginPage;
     protected UsersReadAllPage usersReadAllPage;
 
-    @BeforeAll
+    @BeforeClass
     public static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
@@ -34,7 +34,7 @@ public class BaseTest {
         Configuration.browserCapabilities = options;
     }
 
-    @BeforeEach
+    @BeforeMethod
     public void initPages() {
         logger.info("Инициализация объектов страниц перед тестом.");
         loginPage = new LoginPage();
