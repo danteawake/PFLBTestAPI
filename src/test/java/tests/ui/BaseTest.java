36ee794ac935bd81eee1c92c7537d7779e5429fa
Oleg Egorov <danteawake88@gmail.com>
2026-06-20 08:47:09 +0400

    BaseTest для jenkins

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
 import pages.AllPostPage;
 import pages.LoginPage;
 import pages.UsersReadAllPage;
@@ -23,19 +24,41 @@ public class BaseTest {
     protected AllPostPage allPostPage;
 
     @BeforeClass
-    public static void setUp() {
+    public void setUp() {  // Убрали static
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "http://82.142.167.37:4881/";
         Configuration.browser = "chrome";
-        Configuration.headless = false;
-        Configuration.clickViaJs = true;
         Configuration.timeout = 10000;
-        Configuration.browserSize = "1920x1080";
+        Configuration.clickViaJs = true;
+
+        // Управляем headless через системное свойство из Jenkins
+        boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        Configuration.headless = isHeadless;
+
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--incognito");
+
+        if (isHeadless) {
+            options.addArguments("--headless=new");
+        }
+
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-setuid-sandbox");
+        options.addArguments("--disable-crash-reporter");
         options.addArguments("--disable-notifications");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--window-size=1920,1080");
+
+        // Дополнительные полезные настройки
+        options.setAcceptInsecureCerts(true);
+
         Configuration.browserCapabilities = options;
+        Configuration.browserSize = "1920x1080"; // оставляем для совместимости
+
+        logger.info("Selenide настроен. Headless = " + isHeadless);
     }
 
     @BeforeMethod
