7867004e76ccd08d90f3f35c28ada8b0359f6d55
Oleg Egorov <danteawake88@gmail.com>
2026-06-20 10:34:06 +0400

    ИСПРАВЛЕНИЕ: Динамическая настройка опций браузера 2

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -7,6 +7,10 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.MutableCapabilities;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.AllPostPage;
@@ -28,14 +32,13 @@ public class BaseTest {
     protected AllPostPage allPostPage;
 
     @BeforeClass
-    public void setUp() {  // Убрали static
+    public void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "http://82.142.167.37:4881/";
 
         // Читаем браузер из Jenkins, если не передан — используем chrome по умолчанию
-        String browserFromJenkins = System.getProperty("browser", "chrome");
-        Configuration.browser = browserFromJenkins;
+        Configuration.browser = System.getProperty("browser", "chrome");
 
         Configuration.timeout = 10000;
         Configuration.clickViaJs = true;
@@ -61,29 +64,60 @@ public class BaseTest {
 
         logger.info("Используется пользователь для тестов: " + testUsername);
 
-        ChromeOptions options = new ChromeOptions();
+        // === ДИНАМИЧЕСКАЯ НАСТРОЙКА ОПЦИЙ БРАУЗЕРА (Chrome, Firefox, Edge, Safari) ===
+        MutableCapabilities options;
+        String currentBrowser = Configuration.browser.toLowerCase();
 
-        if (isHeadless) {
-            options.addArguments("--headless=new");
+        if (currentBrowser.contains("firefox")) {
+            FirefoxOptions fxOptions = new FirefoxOptions();
+            if (isHeadless) {
+                fxOptions.addArguments("-headless");
+            }
+            fxOptions.addArguments("--window-size=1920,1080");
+            options = fxOptions;
+
+        } else if (currentBrowser.contains("edge")) {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            if (isHeadless) {
+                edgeOptions.addArguments("--headless=new");
+            }
+            edgeOptions.addArguments("--no-sandbox");
+            edgeOptions.addArguments("--disable-dev-shm-usage");
+            edgeOptions.addArguments("--disable-gpu");
+            edgeOptions.addArguments("--window-size=1920,1080");
+            options = edgeOptions;
+
+        } else if (currentBrowser.contains("safari")) {
+            SafariOptions safariOptions = new SafariOptions();
+            // Safari не поддерживает headless и кастомные аргументы командной строки
+            options = safariOptions;
+
+        } else {
+            // Для chrome и всего остального по умолчанию
+            ChromeOptions chOptions = new ChromeOptions();
+            if (isHeadless) {
+                chOptions.addArguments("--headless=new");
+            }
+            chOptions.addArguments("--no-sandbox");
+            chOptions.addArguments("--disable-dev-shm-usage");
+            chOptions.addArguments("--disable-gpu");
+            chOptions.addArguments("--disable-extensions");
+            chOptions.addArguments("--disable-setuid-sandbox");
+            chOptions.addArguments("--disable-crash-reporter");
+            chOptions.addArguments("--disable-notifications");
+            chOptions.addArguments("--disable-blink-features=AutomationControlled");
+            chOptions.addArguments("--window-size=1920,1080");
+            options = chOptions;
         }
 
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-setuid-sandbox");
-        options.addArguments("--disable-crash-reporter");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-blink-features=AutomationControlled");
-        options.addArguments("--window-size=1920,1080");
-
         // Дополнительные полезные настройки
-        options.setAcceptInsecureCerts(true);
+        options.setCapability("acceptInsecureCerts", true);
+        options.setCapability("unhandledPromptBehavior", "accept");
 
         Configuration.browserCapabilities = options;
         Configuration.browserSize = "1920x1080"; // оставляем для совместимости
 
-        logger.info("Selenide настроен. Headless = " + isHeadless);
+        logger.info("Selenide настроен. Браузер = " + Configuration.browser + ", Headless = " + isHeadless);
     }
 
     @BeforeMethod
